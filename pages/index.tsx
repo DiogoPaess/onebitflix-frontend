cbc7c68aa415e22f49d3a2b6779c97ecb56e6d1e
Diogo Paes <diogo.1@hotmail.com>
2024-04-17 18:21:09 +0100

    completion of the genetic header, visual style and ist responsiveness.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ import styles from "../styles/HomeNoAuth.module.scss";
 import SlideSection from "../src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "../src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "../src/components/common/footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -15,6 +17,10 @@ interface IndexPageProps {
 }
 
 const HomeNoAuth = function ({ course }: IndexPageProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
       <Head>
@@ -27,12 +33,20 @@ const HomeNoAuth = function ({ course }: IndexPageProps) {
         />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div
+          className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection />
-        <SlideSection newestCourses={course} />
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
